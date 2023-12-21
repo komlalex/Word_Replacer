734087edde32e01ab7117e4ccf1b71983f378715
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 09:33:15 +0000

    Initial Commit

---
+++ word_replacer.py
@@ -0,0 +1,7 @@
+def replace_word():
+    text = "Hi guys, I am Alexander and hi hi hi"
+    word_to_replace = print('Enter a word to replace:')
+    new_word = print("Enter new word: ")
+
+    print(text.replace(word_to_replace, new_word))
+

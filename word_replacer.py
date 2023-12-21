b86d9055a72dec4be555b8cadd023bbd4c674e6b
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 09:42:20 +0000

    Final Commit

--- word_replacer.py
+++ word_replacer.py
@@ -1,7 +1,8 @@
 def replace_word():
     text = "Hi guys, I am Alexander and hi hi hi"
-    word_to_replace = print('Enter a word to replace:')
-    new_word = print("Enter new word: ")
+    word_to_replace = input('Enter a word to replace:')
+    new_word = input("Enter new word: ")
 
     print(text.replace(word_to_replace, new_word))
 
+replace_word() 
